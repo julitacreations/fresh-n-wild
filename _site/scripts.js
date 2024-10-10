774e7c9dcdbe264374ce013e141acda948d911ca
julitacreations <90911265+julitacreations@users.noreply.github.com>
2024-10-10 15:35:10 +0200

    accordion js added

--- _site/scripts.js
+++ _site/scripts.js
@@ -6,4 +6,23 @@ $( document ).ready(function() {
         $(this).toggleClass('active');
       });
 
+
+      
+       // Accordion
+    $('.accordion-header').click(function(){
+      // Toggle visibility of the content when the header is clicked
+      $(this).next('.accordion-content').slideToggle();
+      // Toggle active class to highlight active section
+      $(this).toggleClass('active');
+      // Toggle the + and - buttons
+      $(this).find('::after').html($(this).hasClass('active') ? '-' : '+');
+      // Hide other content when a new section is opened
+      $('.accordion-content').not($(this).next('.accordion-content')).slideUp();
+      // Remove active class from other headers
+      $('.accordion-header').not(this).removeClass('active');
+      // Reset the + and - signs on other headers
+      $('.accordion-header').not(this).find('::after').html('+');
+    });
+
+
 });
