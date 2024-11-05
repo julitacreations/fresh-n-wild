bc4c01a808e42655c1025b866d78ee96b65939e0
julitacreations <90911265+julitacreations@users.noreply.github.com>
2024-11-05 14:19:22 +0200

    trying to get slick to work

--- slick.js
+++ slick.js
@@ -1,37 +1,47 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
+if (typeof jQuery === 'undefined') {
+    console.error('jQuery is not loaded! Include jQuery before slick.js.');
+  } else {
+    console.log('jQuery is loaded!');
     
-
-    $('.slider-container').slick({
-        autoplay: true, // Do we want it to autoplay? true or false
-        autoplaySpeed: 2000, // How long between each slide when auto-playing
-        speed: 500, // How fast is the transition in milliseconds
-        arrows: true, // Do you want to show arrows to trigger each slide
-        accessibility: true, // Enables keyboard tabbing and arrow key navigation
-        dots: true, // Enables the dots below to show how many slides
-        fade: false, // Changes the animate from slide to fade if true
-        infinite: true, // When true, means that it will scroll in a circle
-        pauseOnHover: true, // When true means the autoplay pauses when hovering
+    $(document).ready(function(){
+      console.log('Document ready in slick.js!');
+      
+      $('.slider-container').slick({
+        autoplay: true,        // Do we want it to autoplay? true or false
+          autoplaySpeed: 2000,   // How long between each slide when autoplaying
+          speed: 500,            // How fast is the transition 
+        arrows: true,          // Do you want to show arrows to trigger each slide
+        accessibility: true,   // Enables tabbing and arrow key navigation 
+        dots: true,            // Enables the dots below to show how many slides
+        fade: false,           // Changes the animate from slide to fade if true
+        infinite: true,       // When true, means that it will scroll in a circle
+        pauseOnHover: true,   // When true means the autoplay pauses when hovering
         pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
         slidesToShow: 3,
         slidesToScroll: 1,
         responsive: [
-        {
+          {
             breakpoint: 768,
             settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
-        }
+          }
         ]
- 
-        $('.multiple-items').slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 3
-          });
-
-
     });
+    
+    
+    
+    });
+      
+      console.log('Slick slider initialized!');
+    });
+  }
+  
 
-});
+
+
+
+
+
