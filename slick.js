db611692ac2121d8d8c3b20d5d0b81f06efb0d72
julitacreations <90911265+julitacreations@users.noreply.github.com>
2024-11-05 13:37:58 +0200

    testing slick slider

---
+++ slick.js
@@ -0,0 +1,37 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+
+    $('.slider-container').slick({
+        autoplay: true, // Do we want it to autoplay? true or false
+        autoplaySpeed: 2000, // How long between each slide when auto-playing
+        speed: 500, // How fast is the transition in milliseconds
+        arrows: true, // Do you want to show arrows to trigger each slide
+        accessibility: true, // Enables keyboard tabbing and arrow key navigation
+        dots: true, // Enables the dots below to show how many slides
+        fade: false, // Changes the animate from slide to fade if true
+        infinite: true, // When true, means that it will scroll in a circle
+        pauseOnHover: true, // When true means the autoplay pauses when hovering
+        pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
+ 
+        $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+          });
+
+
+    });
+
+});
