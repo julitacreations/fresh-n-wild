822aeacdbcdeda230345a62ec04392636047c1a5
julitacreations <90911265+julitacreations@users.noreply.github.com>
2024-11-05 17:07:27 +0200

    mobile responsive classes added

--- slick.js
+++ slick.js
@@ -7,36 +7,32 @@ if (typeof jQuery === 'undefined') {
     $(document).ready(function(){
       console.log('Document ready in slick.js!');
       
-      $('.slider-container').slick({
-        autoplay: true,        // Do we want it to autoplay? true or false
-          autoplaySpeed: 2000,   // How long between each slide when autoplaying
-          speed: 500,            // How fast is the transition 
-        arrows: true,          // Do you want to show arrows to trigger each slide
-        accessibility: true,   // Enables tabbing and arrow key navigation 
-        dots: true,            // Enables the dots below to show how many slides
-        fade: false,           // Changes the animate from slide to fade if true
-        infinite: true,       // When true, means that it will scroll in a circle
-        pauseOnHover: true,   // When true means the autoplay pauses when hovering
-        pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-    });
-    
-    
+        $('.slider-container').slick({
+            autoplay: true,        // Do we want it to autoplay? true or false
+            autoplaySpeed: 2500,   // How long between each slide when autoplaying
+            speed: 500,            // How fast is the transition 
+            arrows: true,          // Do you want to show arrows to trigger each slide
+            accessibility: true,   // Enables tabbing and arrow key navigation 
+            dots: true,            // Enables the dots below to show how many slides
+            fade: false,           // Changes the animate from slide to fade if true
+            infinite: true,       // When true, means that it will scroll in a circle
+            pauseOnHover: true,   // When true means the autoplay pauses when hovering
+            pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                }
+            ]
+        });
     
     });
       
-      console.log('Slick slider initialized!');
-    });
   }
   
 
